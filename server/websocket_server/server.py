46bdb992dff43b9b61fe10318df67bd51fbf9e6f
Nathaniel Stewart <nathaniel.stewart118@gmail.com>
2025-04-21 04:16:05 -0700

    changed ws server init print msg

--- server/websocket_server/server.py
+++ server/websocket_server/server.py
@@ -8,5 +8,5 @@ load_dotenv()
 
 async def run_websocket_server():
     async with websockets.serve(websocket_handler, os.getenv("SOCKET_SERVER_URL"), int(os.getenv("SOCKET_SERVER_PORT"))):
-        print("WebSocket server running at ws://localhost:8765")
+        print(f"WebSocket server running at ws://{os.getenv("SOCKET_SERVER_URL")}:{os.getenv("SOCKET_SERVER_PORT")}")
         await asyncio.Future()  # run forever
